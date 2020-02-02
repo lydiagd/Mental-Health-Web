ff0352b406936bac5a86d604a163d30c53d349e5
Eric Su <ericsu14@hotmail.com>
2020-02-01 22:15:09 -0800

    Bootstrapped logform and added database functionality

---
+++ public/logform.js
@@ -0,0 +1,50 @@
+var firestore = firebase.firestore();
+
+/* Retrives the user's UUID */
+function getUserID ()
+{
+    return firebase.auth().currentUser.uid;
+}
+
+/* Stories doc-ref */
+const storiesRef = firestore.collection ("stories");
+/* Form elements in LogForm.html */
+const tagSelectField = document.querySelector ('#tag-select');
+const happinessField = document.querySelector ('#happiness');
+const descriptionField = document.querySelector ('#description');
+const button = document.querySelector ('#submit');
+
+button.addEventListener ("click", function () {
+    const tag = tagSelectField.value;
+    const happiness = happinessField.value;
+    const description = descriptionField.value;
+    const uuid = getUserID();
+    const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+
+    /* NULL check */
+    if (!tag || !happiness || !description || !uuid || !timestamp)
+    {
+        alert ("At least one or more fields are empty.");
+        return;
+    }
+
+    /* Ensures that happiness is between one and ten */
+    if (! (happiness >= 1 && happiness <= 10))
+    {
+        alert ("Your happiness rating must be between one and ten.");
+        return;
+    }
+
+    storiesRef.add ({
+        user_id: uuid,
+        happiness: happiness,
+        description: description,
+        timestamp: timestamp,
+        tag: tag
+    }).then (function() { 
+        console.log ("SUCCESS!");
+        window.location.href = '/main.html';
+    }).catch (function (error) {
+        console.log (error);
+    });
+});
